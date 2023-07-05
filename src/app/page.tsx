9bbbf82c0069dfd4d596ae79459c36e2050c0791
Gerson Lamela <gersonlamela7@gmail.com>
2023-07-05 22:03:27 +0100

    feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from 'next/image';
+'use client';
+
+import {signIn, signOut, useSession} from 'next-auth/react';
 
 export default function Home() {
-	return <h1>Hello World!!!</h1>;
+	return (
+		<div>
+			<h1>Hello World!!!</h1>
+		</div>
+	);
 }
