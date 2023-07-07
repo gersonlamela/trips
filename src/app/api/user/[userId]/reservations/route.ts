f7994727d01fe8bb397a6409e7e29ea1df07ba7b
Gerson Lamela <gersonlamela7@gmail.com>
2023-07-07 19:17:23 +0100

    feat: add page reload on cancel

--- src/app/api/user/[userId]/reservations/route.ts
+++ src/app/api/user/[userId]/reservations/route.ts
@@ -4,21 +4,27 @@ import {NextResponse} from 'next/server';
 export async function GET(request: Request, {params: {userId}}: {params: {userId: string}}) {
 	const {searchParams} = new URL(request.url);
 
+	console.log({userId});
+
 	if (!userId) {
 		return {
 			status: 400,
 			body: {
-				message: 'Missing user',
+				message: 'Missing userId',
 			},
 		};
 	}
 
 	const reservations = await prisma.tripReservation.findMany({
-		where: {userId: userId},
+		where: {
+			userId: userId,
+		},
 		include: {
 			trip: true,
 		},
 	});
+
+	console.log({reservations});
 
 	return new NextResponse(JSON.stringify(reservations), {status: 200});
 }
--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -1,35 +1,57 @@
 'use client';
 
-import {getServerSession} from 'next-auth';
+import React, {useEffect, useState} from 'react';
 import {useSession} from 'next-auth/react';
 import {useRouter} from 'next/navigation';
-import {useEffect, useState} from 'react';
+import {Prisma} from '@prisma/client';
+import Link from 'next/link';
 
-import {TripReservation} from '@prisma/client';
+import Button from '@/components/Button';
+import UserReservationItem from './components/UserReservationItem';
 
-export default function MyTrips() {
+const MyTrips = () => {
+	const [reservations, setReservations] = useState<
+		Prisma.TripReservationGetPayload<{
+			include: {trip: true};
+		}>[]
+	>([]);
+
 	const {status, data} = useSession();
-
-	const [reservations, setReservations] = useState<TripReservation[]>([]);
 
 	const router = useRouter();
 
+	const fetchReservations = async () => {
+		const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any)?.id}/reservations`);
+
+		const json = await response.json();
+
+		setReservations(json);
+	};
+
 	useEffect(() => {
-		if (status === 'unauthenticated' || !data?.user) {
+		if (status === 'unauthenticated') {
 			return router.push('/');
 		}
 
-		const fetchReservations = async () => {
-			const response = await fetch(`http://localhost:3000/api/user/${(data?.user as any).id}/reservations`);
-			const json = await response.json();
-
-			setReservations(json);
-		};
-
 		fetchReservations();
-	}, [status, data?.user, router]);
+	}, [status]);
 
-	console.log(reservations);
+	return (
+		<div className="container mx-auto p-5">
+			<h1 className="font-semibold text-primaryDarker text-xl">Minhas Viagens</h1>
+			{reservations.length > 0 ? (
+				reservations?.map((reservation) => <UserReservationItem fetchReservations={fetchReservations} key={reservation.id} reservation={reservation} />)
+			) : (
+				<div className="flex flex-col">
+					<p className="mt-2 font-medium text-primaryDarker">Ainda não tem nenhuma reserva! =(</p>
 
-	return <h1></h1>;
-}
+					<Link href="/">
+						<Button className="w-full mt-2">Fazer reserva</Button>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default MyTrips;
