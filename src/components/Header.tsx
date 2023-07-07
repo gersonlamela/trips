cc56a0a563293698b0ad442309f95a0527d4decf
Gerson Lamela <gersonlamela7@gmail.com>
2023-07-07 18:34:01 +0100

    feat: add my trips to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,7 @@ import {AiOutlineMenu} from 'react-icons/ai';
 
 export function Header() {
 	const [menuIsOpen, setMenuIsOpen] = useState(false);
+
 	const {status, data} = useSession();
 	const handleLoginClick = () => signIn();
 	const handleLogoutClick = () => {
@@ -37,8 +38,11 @@ export function Header() {
 					<Image src={data.user.image!} width={35} height={35} alt={data.user.name!} className="rounded-full shadow-md" />
 
 					{menuIsOpen && (
-						<div className="z-50 absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
-							<button className="text-primary text-sm font-semibold" onClick={handleLogoutClick}>
+						<div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+							<Link href="/my-trips" onClick={() => setMenuIsOpen(false)}>
+								<button className="text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold">Minhas Viagens</button>
+							</Link>
+							<button className="text-primary  pt-2 text-sm font-semibold" onClick={handleLogoutClick}>
 								Logout
 							</button>
 						</div>
