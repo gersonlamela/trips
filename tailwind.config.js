e5b96dc96fbf44b31105def49cf9f2d1fd82eba5
Gerson Lamela <gersonlamela7@gmail.com>
2023-07-07 12:34:52 +0100

    feat: add footer components

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
 				primaryDarker: '#312A4F',
 				grayPrimary: '#717171',
 				grayLighter: '#BBBFBF',
+				walterWhite: '#f5f5f5',
 			},
 			textColor: {
 				dark: '#717171',
