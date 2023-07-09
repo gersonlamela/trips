59281b2e94e607e030274e72882dd4861c16566a
Gerson Lamela <gersonlamela7@gmail.com>
2023-07-09 09:10:36 +0100

    feat: add trip search

---
+++ src/app/trips/search/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import TripItem from '@/components/TripItem';
+
+import {Trip} from '@prisma/client';
+import {useSearchParams} from 'next/navigation';
+import {useEffect, useState} from 'react';
+
+export default function TripsSearch() {
+	const [trips, setTrips] = useState<Trip[]>([]);
+	const searchParams = useSearchParams();
+
+	useEffect(() => {
+		const fetchTrips = async () => {
+			const response = await fetch(`/api/trips/search?text=${searchParams.get('text') ?? ''}&startDate=${searchParams.get('startDate')}&budget=${searchParams.get('budget')}`);
+
+			const data = await response.json();
+
+			console.log(data);
+
+			setTrips(data);
+		};
+
+		fetchTrips();
+	}, [searchParams]);
+
+	return (
+		<div className="container mx-auto flex flex-col items-center p-5">
+			<h1 className="text-primaryDarker font-semibold text-xl ">Hospedagens Encontradas</h1>
+			<h2 className="text-grayPrimary font-medium  mb-5">{trips.length > 0 ? 'Listamos as melhores viagens para ti!' : 'Não encontramos nenhuma viagem =('}</h2>
+			<div className="flex flex-col gap-4">{trips?.map((trip) => <TripItem key={trip.id} trip={trip} />)}</div>
+		</div>
+	);
+}
