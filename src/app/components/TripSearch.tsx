71f7168d279c91a3c265a60a5ba237c6898f0323
Gerson Lamela <gersonlamela7@gmail.com>
2023-07-14 19:24:45 +0100

    style:adapted home page to desktop

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -29,12 +29,12 @@ const TripSearch = () => {
 	};
 
 	return (
-		<div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
-			<h1 className="font-semibold text-2xl text-primaryDarker text-center">
+		<div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
+			<h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
 				Encontre sua próxima <span className="text-primary">viagem!</span>
 			</h1>
 
-			<div className="flex flex-col gap-4 mt-5">
+			<div className="lg:mt-12 flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:bg-opacity-20 lg:rounded-lg">
 				<Input
 					placeholder="Onde você quer ir?"
 					error={!!errors.text}
@@ -47,13 +47,15 @@ const TripSearch = () => {
 					})}
 				/>
 
-				<div className="flex gap-4">
+				<div className="flex gap-4 lg:w-full">
 					<Controller name="startDate" control={control} render={({field}) => <DatePicker placeholderText="Data Inicio" onChange={field.onChange} selected={field.value} className="w-full" minDate={new Date()} />} />
 
 					<Controller name="budget" control={control} render={({field}) => <CurrencyInput allowDecimals={false} placeholder="Orçamento" onValueChange={field.onChange as any} value={field.value} onBlur={field.onBlur} />} />
 				</div>
 
-				<Button onClick={() => handleSubmit(onSubmit)()}>Buscar</Button>
+				<Button onClick={() => handleSubmit(onSubmit)()} className="lg:w-1/2">
+					Buscar
+				</Button>
 			</div>
 		</div>
 	);
