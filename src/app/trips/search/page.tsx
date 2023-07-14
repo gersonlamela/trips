49688e47f565651bbaaa3e26fd4516539099851b
Gerson Lamela <gersonlamela7@gmail.com>
2023-07-14 19:28:19 +0100

    style:adapted search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -25,10 +25,10 @@ export default function TripsSearch() {
 	}, [searchParams]);
 
 	return (
-		<div className="container mx-auto flex flex-col items-center p-5">
-			<h1 className="text-primaryDarker font-semibold text-xl ">Hospedagens Encontradas</h1>
-			<h2 className="text-grayPrimary font-medium  mb-5">{trips.length > 0 ? 'Listamos as melhores viagens para ti!' : 'Não encontramos nenhuma viagem =('}</h2>
-			<div className="flex flex-col gap-4">{trips?.map((trip) => <TripItem key={trip.id} trip={trip} />)}</div>
+		<div className="container mx-auto flex flex-col items-center lg:items-start p-5">
+			<h1 className="text-primaryDarker font-semibold lg:w-full lg:text-left text-xl lg:text-[2.5rem] ">Hospedagens Encontradas</h1>
+			<h2 className="text-grayPrimary font-medium  mb-5 lg:mt-6 lg:w-full lg:text-left">{trips.length > 0 ? 'Listamos as melhores viagens para ti!' : 'Não encontramos nenhuma viagem =('}</h2>
+			<div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6">{trips?.map((trip) => <TripItem key={trip.id} trip={trip} />)}</div>
 		</div>
 	);
 }
